94933910ae1c1405d653e2a81bf21d0248fb34d3
Ignacio <andersonevangelistaignacio@gmail.com>
2025-11-21 23:39:14 +0000

    2comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 100):
    f = f"{number:02}" if number < 10 else str(number)
-   print("{}" .format(f), end=', ' if number != 99 else '\n')
+   print("{}".format(f), end=', ' if number != 99 else '\n')
