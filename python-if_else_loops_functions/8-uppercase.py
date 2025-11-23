987f8f850a08947d175d466457938be4b9a71efa
Ignacio <andersonevangelistaignacio@gmail.com>
2025-11-23 19:30:49 +0000

    8-upc

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,5 @@ def uppercase(str):
        texto_mudado += chr(ord(c) - 32)
       else:
         texto_mudado += c
-    print("{}".format(texto_mudado))
+    print("{}".format(texto_mudado), end="")
 uppercase('')
