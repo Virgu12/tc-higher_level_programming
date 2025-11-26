a1a4927ec70a0123ceceb08dd9a2c729e0828c29
Ignacio <andersonevangelistaignacio@gmail.com>
2025-11-26 21:28:04 +0000

    12fizzbuzz12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0: 
+        if number % 3 == 0 and number % 5 == 0:
             saida = "FizzBuzz"
         elif number % 3 == 0:
             saida = "Fizz"
         elif number % 5 == 0:
-            saida= "Buzz"
+            saida = "Buzz"
         else:
             saida = str(number)
         print(saida, end=" ")
