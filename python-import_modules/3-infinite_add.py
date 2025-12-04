02a0327afb69b4d16f3b892a2e8d95adfdc8f497
Ignacio <andersonevangelistaignacio@gmail.com>
2025-12-04 21:26:09 +0000

    3-infinite_add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     numsm = 0
     for nmr in range(1, len(sys.argv)):
-        numsm += int(sys.argv[nmr]) 
+        numsm += int(sys.argv[nmr])
     print(numsm)
