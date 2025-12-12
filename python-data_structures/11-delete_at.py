53ecc0d9f131399c7e9d95a686c6a566064baf14
Ignacio <andersonevangelistaignacio@gmail.com>
2025-12-12 18:54:19 +0000

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = []
+    if  idx < 0 or idx >= len(my_list):
+        return my_list
     for nm in range(len(my_list)):
         if idx != nm:
             new_list.append(my_list[nm])
-        else:
-            pass
     return new_list
