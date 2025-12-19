944a1cdc960ea1828df155dfe0487eeffc01cf04
Ignacio <andersonevangelistaignacio@gmail.com>
2025-12-19 15:32:02 +0000

    square_matrix_simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for x in matrix:
         new_matrix.append(list(map(lambda x: x ** 2, x)))
-    return new_matrix    
+    return new_matrix
