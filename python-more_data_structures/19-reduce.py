f711b188d9faf1595cef6b9efdecb18bd643e779
Ignacio <andersonevangelistaignacio@gmail.com>
2025-12-28 20:36:26 +0000

    rdc

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -3,6 +3,8 @@ from functools import reduce
 
 
 def calc_average(a_dictionary):
-   average_salary = reduce(lambda x, y: x + y[1]['salary'], a_dictionary.items(), 0) / len(a_dictionary)
-   average_age = reduce(lambda x, y: x + y[1]['age'], a_dictionary.items(), 0) / len(a_dictionary)
+   total_salary = reduce(lambda x, y: x + y['salary'], a_dictionary, 0)
+   total_age = reduce(lambda x, y: x + y['age'], a_dictionary, 0)
+   average_salary = total_salary / len(a_dictionary)
+   average_age = total_age / len(a_dictionary)
    return average_salary, average_age
