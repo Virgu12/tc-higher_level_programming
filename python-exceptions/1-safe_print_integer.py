ad8f7a15cd20f9559adf005f5bae3607170fac46
Ignacio <andersonevangelistaignacio@gmail.com>
2026-01-03 01:11:42 +0000

    safe_printZ

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,20 +6,3 @@ def safe_print_integer(value):
         return True
     except ValueError:
         return False
-    
-
-
-value1 = 89
-has_been_print = safe_print_integer(value1)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value2 = -89
-has_been_print = safe_print_integer(value2)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
