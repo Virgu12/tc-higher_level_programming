94398e15db32c667cbc3e63b899b74e57779ed0d
Ignacio <andersonevangelistaignacio@gmail.com>
2026-01-15 18:52:26 +0000

    say_my_nameDOCT

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -25,7 +25,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError(name_errorF)
     if not isinstance(last_name, str):
         raise TypeError(name_errorL)
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+    print(f"My name is {first_name} {last_name}")
