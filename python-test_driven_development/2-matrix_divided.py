f789679b1527755ab9c07832f315b5a9248d41d5
Ignacio <andersonevangelistaignacio@gmail.com>
2026-01-15 14:20:57 +0000

    matrix_div_test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,8 @@ Module provides a function that divides a matrix by the indicated value
 Includes type checking and handle float numbers
 
 """
+
+
 def matrix_divided(matrix, div):
     """
     adds two objects
@@ -13,13 +15,14 @@ def matrix_divided(matrix, div):
 
     return:
         the matrix made of list of lists divided by "div"
-    
+
     raise:
-        TypeError: if div or matrix not int or float, if matrix sizes are not equal, matrix not a list of lists.
+        TypeError: if div or matrix not int or float,
+                    if matrix sizes are not equal,
+                        matrix not a list of lists.
         ZeroDivisionError: if div == 0
     """
     result = []
-    
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
@@ -28,22 +31,22 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+    matrixlist = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list) or len(matrix) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+        raise TypeError(matrixlist)
+
     if not all(isinstance(x, list) for x in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(matrixlist)
 
     for x in matrix:
         row_matrix = len(matrix[0])
         if len(x) != row_matrix:
             raise TypeError("Each row of the matrix must have the same size")
-        
+
         for y in x:
             if not isinstance(y, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                
+                raise TypeError(matrixlist)
+
         matrix_result = list(map(lambda y: round(y / div, 2), x))
         result.append(matrix_result)
 
