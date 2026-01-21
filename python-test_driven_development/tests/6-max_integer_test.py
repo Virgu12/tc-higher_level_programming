afd20743a4dce2f198378308ee0bc49fe3dbf0dc
Ignacio <andersonevangelistaignacio@gmail.com>
2026-01-21 00:22:39 +0000

    max

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,2 +1,46 @@
 #!/usr/bin/python3
 import unittest
+
+class test_max_integer(unittest.TestCase):
+    
+    def test_if_list_is_empty(self):
+        self.assertIsNone(max_integer([]))
+
+    
+    def test_integer_max(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    
+
+    def test_only_one_integer(self):
+        self.assertEqual(max_integer([5]), 5)
+    
+    def test_with_inf(self):
+        self.assertEqual(max_integer([100000, 109, float("inf")]), float("inf"))
+
+    def test_with_negative_inf(self):
+        self.assertEqual(max_integer([-10, -500000000000, float("-inf")]), -10)
+    
+
+    def test_str_and_int_in_the_same_list(self):
+        with self.assertRaises(TypeError):
+            max_integer(['a', '4', 'A', 8])
+    
+
+    def test_only_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3, -10]), -1)
+    
+
+    def test_with_floats(self):
+        self.assertEqual(max_integer([1.5, 2.7, 0.3]), 2.7)
+
+
+    def test_only_strings(self):
+        self.assertEqual(max_integer(["a", "z", "b"]), "z")
+
+    def test_list_with_none(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, None, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
